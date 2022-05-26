2ac26ab8f37519db24e2d287ab5cb9cf34ddf3de
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-05-26 13:52:34 -0300

    base

---
+++ ejercicio2/index.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+class Contenedor {
+  constructor(nombre, id, precio) {
+    this.nombre = nombre;
+    this.id = id;
+    this.precio = precio;
+  }
+
+  save() {}
+  getById() {}
+  getAll() {}
+  deletById() {}
+  deletAll() {}
+}
