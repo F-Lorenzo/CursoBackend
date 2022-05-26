5685b08c4f83406072fbf04abffe3f042feb5fdc
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-05-26 15:11:54 -0300

    falta :save,deleteAll y probar

--- ejercicio2/index.js
+++ ejercicio2/index.js
@@ -1,15 +1,34 @@
 import fs from "fs";
 
-class Contenedor {
-  constructor(nombre, id, precio) {
-    this.nombre = nombre;
+const array = [];
+
+class Product {
+  constructor(name, price, id) {
+    this.nombre = name;
     this.id = id;
-    this.precio = precio;
+    this.precio = price;
   }
 
-  save() {}
-  getById() {}
-  getAll() {}
-  deletById() {}
+  save(producto) {}
+  getById(id) {
+    let products = array.filter((product) => product.id === id);
+    return products;
+  }
+  getAll() {
+    let getProducts = async () => {
+      try {
+        const content = await fs.promisis.readFile("./productos.txt", "utf8");
+        const objContent = JSON.parse(content);
+        console.log(objContent);
+        return objContent;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    };
+  }
+  deletById(id) {
+    let products = array.filter((product) => product.id === id);
+    array.delete(products);
+  }
   deletAll() {}
 }
