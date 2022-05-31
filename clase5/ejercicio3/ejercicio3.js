1f5bbc47143af8a52576d42281f0c2171ed1e632
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-05-31 14:12:51 -0300

    .

--- clase5/ejercicio3/ejercicio3.js
+++ clase5/ejercicio3/ejercicio3.js
@@ -1,10 +1,10 @@
 import moment from "moment";
 
-// let fecha = moment().format("YYYY-MM-DD");
-
-// let naciemiento = moment([1999, 12, 7]);
-
-// let diferecia = fecha.diff(naciemiento);
-
-// console.log(diferecia);
-const now = moment();
+let fecha = moment().format("YYYY-MM-DD");
+let nacimiento = moment(["1999, 12, 7"]);
+let difereciaAnual = fecha.diff(nacimiento, "years");
+let difereciaDiaria = fecha.diff(nacimiento, "Days");
+console.log("Naci en la fecha " + nacimiento);
+console.log("Hoy es " + fecha);
+console.log("pasaron " + difereciaAnual + " años desde que naci");
+console.log("pasaron " + difereciaDiaria + " dias desde que naci");
