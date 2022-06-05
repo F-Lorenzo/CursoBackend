6e767b92e229c6a4fb20d45851e9eaec5a240505
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-06-05 16:35:03 -0300

    entrega 3

--- entregas/ejercicio3/server.js
+++ entregas/ejercicio3/server.js
@@ -4,10 +4,13 @@ import fs from "fs";
 const PORT = 8080;
 const app = express();
 
+/* Reading the file and parsing it to JSON. */
 const baseDatos = JSON.parse(fs.readFileSync("./productos.txt", "utf-8"));
 
+/* Mapping the baseDatos array and returning the same array. */
 const listaProductos = baseDatos.map((productos) => productos);
 
+/* A function that returns the list of products. */
 app.get("/productos", (req, res) => {
   try {
     res.send(listaProductos);
@@ -16,10 +19,13 @@ app.get("/productos", (req, res) => {
   }
 });
 
+/* Getting the length of the array and then generating a random number between 0 and the length of the
+array. Then it is getting the product at the random index. */
 const max = parseInt(listaProductos.length);
 const randomNum = parseInt(Math.floor(Math.random() * max));
 const productoRandom = listaProductos[randomNum];
 
+/* Getting the product at the random index. */
 app.get("/productoRandom", (req, res) => {
   try {
     res.send(productoRandom);
@@ -28,6 +34,7 @@ app.get("/productoRandom", (req, res) => {
   }
 });
 
+/* Listening to the server and if there is an error it will log it. */
 const server = app.listen(PORT, () => {
   console.log(`server funcionando en port http://localhost:${PORT}`);
 });
