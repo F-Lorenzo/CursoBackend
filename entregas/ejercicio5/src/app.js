a9307ad19390f60b6f7972d6f12189f4a3192b26
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-06-27 16:25:03 -0300

    avance

--- entregas/ejercicio5/src/app.js
+++ entregas/ejercicio5/src/app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = require("./routes.js");
+const ejs = require("ejs");
+const { dirname } = require("path");
+const { fileURLToPath } = require("url");
+
 const app = express();
 const PORT = 8080;
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+/* ----------------------------------- use ---------------------------------- */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/", router);
-
-app.use(express.static("public"));
-app.use("/static", express.static("public"));
-app.set("views", "./views");
+/* ----------------------------------- set ---------------------------------- */
+app.set("views", join(__dirname, "./views"));
 app.set("view engine", "ejs");
 
 const server = app.listen(PORT, () => {
--- entregas/ejercicio5/src/routes.js
+++ entregas/ejercicio5/src/routes.js
@@ -9,72 +9,21 @@ const Productos = require("./productos.js");
 /* -------------------------------------------------------------------------- */
 
 router.get("/", (req, res) => {
-  res.render("index");
+  try {
+    const productos = Productos.getAll().then((data) => {
+      res.send(data);
+    });
+    res.render("index", { productos });
+  } catch (err) {
+    console.log(err);
+  }
 });
-router
-  .route("/productos")
-  .get((req, res) => {
-    try {
-      const productos = Productos.getAll().then((data) => {
-        res.send(data);
-      });
-      res.render("listaProductos");
-    } catch (err) {
-      console.log(err);
-    }
-  })
-  .post((req, res) => {
-    try {
-      const newProducto = req.body;
-      Productos.save(newProducto);
-      res.redirect("/");
-    } catch (err) {
-      console.log(err);
-    }
-  });
 
-router
-  .route("/productos/:id")
-  .get((req, res) => {
-    try {
-      Productos.getById(req.params.id).then((data) => {
-        res.send(data);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  })
-
-  .put((req, res) => {
-    let id = req.params.id - 1;
-    try {
-      const baseDatos = JSON.parse(
-        fs.readFileSync("./listaProductos.json", "utf8")
-      );
-      baseDatos[id]["modelo"] = req.body.modelo;
-      baseDatos[id]["marca"] = req.body.marca;
-      baseDatos[id]["precio"] = req.body.precio;
-      console.log(baseDatos[req.params.id]);
-      fs.writeFileSync(
-        "./listaProductos.json",
-        JSON.stringify(baseDatos, null, 2)
-      );
-      fs.writeFileSync(
-        "./listaProductos.txt",
-        JSON.stringify(baseDatos, null, 2)
-      );
-      res.send(baseDatos);
-    } catch (err) {
-      console.log(err);
-    }
-  })
-  .delete((req, res) => {
-    try {
-      Productos.deleteById(req.params.id - 1);
-      res.sendStatus(200);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-
-module.exports = router;
+router.post("/productos", (req, res) => {
+  try {
+    const newProducto = Productos.Save(req.body);
+    res.redirect("/").render(newProducto);
+  } catch (err) {
+    console.log(err);
+  }
+});
