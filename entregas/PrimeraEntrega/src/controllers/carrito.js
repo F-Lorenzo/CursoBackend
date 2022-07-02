e81400f7459e4f9733a91e796cd75117ec57d42b
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-07-02 13:22:11 -0300

    route productos hecho

--- entregas/PrimeraEntrega/src/controllers/carrito.js
+++ entregas/PrimeraEntrega/src/controllers/carrito.js
@@ -1,3 +1,9 @@
+// POST: '/' - Crea un carrito y devuelve su id.
+// DELETE: '/:id' - Vacía un carrito y lo elimina.
+// GET: '/:id/productos' - Me permite listar todos los productos guardados en el carrito
+// POST: '/:id/productos' - Para incorporar productos al carrito por su id de producto
+// DELETE: '/:id/productos/:id_prod' - Eliminar un producto del carrito por su id de carrito y de producto
+
 import Productos from "./Productos";
 import dbCarrito from "../db/dbCarrito.json";
 
@@ -6,10 +12,12 @@ const data = JSON.parse(fs.readFileSync(dbCarrito, "utf8"));
 export default class Carrito {
   constructor(user, productos, id) {
     this.user = user;
-    this.productos = productos;
     this.id = id;
+    this.productos = [{ productos }];
   }
-  async save(productos) {}
-  async deleteById(id) {}
+  async crete(obj) {}
+  async saveProductos(productos) {}
+  async getProductos(carritoId) {}
+  async deleteById(carritoId) {}
   async deleteAll() {}
 }
--- entregas/PrimeraEntrega/src/routes/routeCarrito.js
+++ entregas/PrimeraEntrega/src/routes/routeCarrito.js
@@ -1,3 +1,9 @@
+// POST: '/' - Crea un carrito y devuelve su id.
+// DELETE: '/:id' - Vacía un carrito y lo elimina.
+// GET: '/:id/productos' - Me permite listar todos los productos guardados en el carrito
+// POST: '/:id/productos' - Para incorporar productos al carrito por su id de producto
+// DELETE: '/:id/productos/:id_prod' - Eliminar un producto del carrito por su id de carrito y de producto
+
 router("/carrito")
   .get((req, res) => {})
   .post((req, res) => {})
