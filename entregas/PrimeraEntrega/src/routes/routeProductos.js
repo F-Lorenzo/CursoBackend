57e550a530d808a9d839f531b87a7f463596fb38
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-07-08 17:18:40 -0300

    cambio de sintaxis en rutas

--- entregas/PrimeraEntrega/src/routes/routeProductos.js
+++ entregas/PrimeraEntrega/src/routes/routeProductos.js
@@ -2,69 +2,68 @@ import Productos from "../controllers/Productos.js";
 import { Router } from "express";
 const routerProductos = new Router();
 
-routerProductos("/productos")
-  .get(":id", (req, res) => {
-    try {
-      let id = req.params.id;
-      if (id) {
-        res.send(Productos.getById(id));
-      }
-      res.send(Productos.getAll());
-    } catch (error) {
-      console.log(error);
+routerProductos.get("/productos/:id", (req, res) => {
+  try {
+    let id = req.params.id;
+    if (id) {
+      res.send(Productos.getById(id));
     }
-  })
-  .post("/?", (req, res) => {
-    try {
-      let user = req.query.user;
-      const userData = JSON.parse(
-        fs.readFileSync("../db/dbUsuarios.js", "utf-8")
+    res.send(Productos.getAll());
+  } catch (error) {
+    console.log(error);
+  }
+});
+routerProductos.post("/productos?", (req, res) => {
+  try {
+    let user = req.query.user;
+    const userData = JSON.parse(
+      fs.readFileSync("../db/dbUsuarios.js", "utf-8")
+    );
+    const usuario = userData.find((usuario) => usuario.name === user);
+    if (usuario.authorized == true) {
+      Productos.save(req.body);
+    } else {
+      res.send("error : -1, descripcion: ruta 'x' método 'y' no autorizada ");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+routerProductos.put("/productos/:id?", (req, res) => {
+  try {
+    let id = req.params.id;
+    let user = req.query.user;
+    const userData = JSON.parse(
+      fs.readFileSync("../db/dbUsuarios.js", "utf-8")
+    );
+    const usuario = userData.find((usuario) => usuario.name === user);
+    if (usuario.authorized == true) {
+      const productData = JSON.parse(
+        fs.readFileSync("../db/dbProductos", "utf-8")
       );
-      const usuario = userData.find((usuario) => usuario.name === user);
-      if (usuario.authorized == true) {
-        Productos.save(req.body);
-      } else {
-        res.send("error : -1, descripcion: ruta 'x' método 'y' no autorizada ");
-      }
-    } catch (error) {
-      console.log(error);
+      const producto = productData.find((p) => p.id === id);
+      producto.modify(req.body); //esto no se si esta muy bien
+    } else {
+      res.send("error : -1, descripcion: ruta 'x' método 'y' no autorizada ");
     }
-  })
-  .put("/:id?", (req, res) => {
-    try {
-      let id = req.params.id;
-      let user = req.query.user;
-      const userData = JSON.parse(
-        fs.readFileSync("../db/dbUsuarios.js", "utf-8")
-      );
-      const usuario = userData.find((usuario) => usuario.name === user);
-      if (usuario.authorized == true) {
-        const productData = JSON.parse(
-          fs.readFileSync("../db/dbProductos", "utf-8")
-        );
-        const producto = productData.find((p) => p.id === id);
-        producto.modify(req.body); //esto no se si esta muy bien
-      } else {
-        res.send("error : -1, descripcion: ruta 'x' método 'y' no autorizada ");
-      }
-    } catch (error) {
-      console.log(error);
+  } catch (error) {
+    console.log(error);
+  }
+});
+routerProductos.delete("/productos/:id?", (req, res) => {
+  try {
+    let user = req.query.user;
+    let id = req.params.id;
+    const userData = JSON.parse(
+      fs.readFileSync("../db/dbUsuarios.js", "utf-8")
+    );
+    const usuario = userData.find((usuario) => usuario.name === user);
+    if (usuario.authorized == true) {
+      res.send(Productos.getById(id));
     }
-  })
-  .delete("/:id?", (req, res) => {
-    try {
-      let user = req.query.user;
-      let id = req.params.id;
-      const userData = JSON.parse(
-        fs.readFileSync("../db/dbUsuarios.js", "utf-8")
-      );
-      const usuario = userData.find((usuario) => usuario.name === user);
-      if (usuario.authorized == true) {
-        res.send(Productos.getById(id));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export { routerProductos };
