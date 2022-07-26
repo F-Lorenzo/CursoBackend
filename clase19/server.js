ca81636f36e4f0b5df432e28e133b3504b041079
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-07-26 11:57:41 -0300

    clases 18-19-21

---
+++ clase19/server.js
@@ -0,0 +1,62 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./db/prueba-coder-8b8f5-firebase-adminsdk-0vylm-128b16343e.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+CRUD();
+
+async function CRUD() {
+  const db = admin.firestore();
+  const query = db.collection("usuarios");
+  try {
+    /* CREATE */
+    const usuarioData = {
+      nombre: "Juan",
+      dni: "1111111",
+    };
+    let doc = query.doc();
+    await doc.create(usuarioData);
+
+    console.log("Usuario creado");
+  } catch (error) {
+    console.log(error);
+  }
+
+  /* READ */
+  // query
+  //   .get()
+  //   .then((snapshot) => {
+  //     snapshot.forEach((doc) => {
+  //       console.log(doc.id, "=>", doc.data());
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log("Error getting documents", err);
+  //   });
+
+  /* UPDATE */
+  // query
+  //   .where("dni", "==", "1111111")
+  //   .get()
+  //   .then((snapshot) => {
+  //     snapshot.forEach((doc) => {
+  //       doc.ref.update({ nombre: "Juanito" });
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log("Error getting documents", err);
+  //   });
+
+  /* DELETE */
+  //   query
+  //     .where("dni", "==", "1111111")
+  //     .get()
+  //     .then((snapshot) => {
+  //       snapshot.forEach((doc) => {
+  //         doc.ref.delete();
+  //       });
+  //     });
+}
---
+++ clase21/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { faker } from "@faker-js/faker";
+const PORT = 8080;
+const app = express();
+let str = "NOMBRE;APELLIDO;COLOR;";
+let id = 1;
+app.get("/test", (req, res) => {
+  const datos = [];
+  for (let i = 0; i < 10; i++) {
+    const dato = {
+      id: id++,
+      nombre: faker.name.firstName(),
+      apellido: faker.name.lastName(),
+      color: faker.color.human(),
+    };
+    datos.push(dato);
+  }
+  res.status(200).json({ datos });
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`server funcionando en port http://localhost:${PORT}`);
+});
+server.on("error", (err) => console.error(err));
